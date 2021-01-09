aa9291d18649683628b2f5053d9076cbd9fdb1c1
Philipp Reiner <phil@q-fx.de>
2021-01-09 20:09:31 +0100

    Fixes and Imprint text

--- components/content/content.js
+++ components/content/content.js
@@ -1,5 +1,5 @@
 // Wraps content
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 import Style from "./content.module.css";
 
 export default function Content({children}) {
@@ -8,5 +8,19 @@ export default function Content({children}) {
     <Box className={Style.content}>
       {children}
     </Box>
+  );
+}
+
+export function Basicpage({children, title}) {
+
+  return (
+    <Container maxW="40em" pt={[4, 6]} pb={[4, 6]}>
+      <Heading pt={[4,12]} pb={[2,4]} as="h1">
+        {title}
+      </Heading>
+      <Content>
+        {children}
+      </Content>
+    </Container>
   );
 }
