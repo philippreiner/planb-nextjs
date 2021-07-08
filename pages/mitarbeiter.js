8c6672ad6ee80bc1b44496d429efd7489f101789
Cedric Döz <cedric.doez@plan-b-gmbh.com>
2021-07-08 12:36:16 +0200

    Added Hero to Mitarbeiter page

--- pages/mitarbeiter.js
+++ pages/mitarbeiter.js
@@ -1,9 +1,13 @@
 import Layout from "@components/layout";
 import Header from "@components/header/header";
 import { Basicpage } from "@components/content/content";
-import { DevNote, SegmentHeading } from "@components/atoms/atoms";
-import { Flex, Box, Center, Link } from "@chakra-ui/react";
+import { DevNote, SegmentHeading, Arrowlink } from "@components/atoms/atoms";
+import { Flex, Box, Center, Link, Heading, Text } from "@chakra-ui/react";
 import { Wallpaper, Main } from "@components/blocks/blocks";
+
+import Hero from "@components/hero/hero";
+import { Slide, Fade } from "react-awesome-reveal";
+
 
 import NextLink from "next/link";
 import NextImage from "next/image";
@@ -11,12 +15,35 @@ import NextImage from "next/image";
 export default function Home() {
     return (
         <Layout>
-            <Header />
-            <Basicpage title="Mitarbeiter">
-                <DevNote>
-                    Erklärt warum arbeiten bei der PlanB toll ist (Und ein paar worte über die firma z.b. standorte), die vorteile des jobs, erwartungen und stellenanzeigen sowie Ausbildungsverweis.
-                </DevNote>
-            </Basicpage>
+            <Hero background="url(images/background-solutions.jpg)">
+                <Fade triggerOnce={true} delay={100}>
+                <Main maxW="full">
+                    <Box pt={[12, 20, 32]} pb={[12, 20, 32]}>
+                    <Heading
+                        maxW="2xl"
+                        as="h1"
+                        pb={[2, 4]}
+                        fontSize={["24px", "36px", "54px", "72px"]}
+                        textTransform="uppercase"
+                        color="white"
+                    >
+                        Agiles arbeiten &amp; besonderheiten der Plan.B
+                    </Heading>
+                    <Box w="100px" bg="white" h="6px" my="2" mb={[4, 6]}></Box>
+                    <Text
+                        pb={[2, 4, 6]}
+                        maxW={[null, null, "80%", "45%"]}
+                        color="white"
+                    >
+                        Wir sind 130 kluge Köpfe für die digitale Transformation und Zukunft der Zusammenarbeit.
+                        Jeden Tag arbeiten wir an den digitalen Produkten, innovativen Geschäftsmodellen, 
+                        skalierbaren Cloud-Architekturen und der Zufriedenheit von hunderttausenden Menschen bei Ihrer Arbeit.
+                    </Text>
+                    <Arrowlink label="Die Menschen hinter PlanB." url="/about" />
+                    </Box>
+                </Main>
+                </Fade>
+            </Hero>
         </Layout>
     );
 }
