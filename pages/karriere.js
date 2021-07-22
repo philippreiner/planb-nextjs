088ff4def522672d11e393e605b4bb320e0742c0
Cedric Döz <cedric.doez@plan-b-gmbh.com>
2021-07-22 11:23:16 +0200

    work in progress

--- pages/karriere.js
+++ pages/karriere.js
@@ -46,7 +46,7 @@ export default function Home() {
       <Hero background="url(images/karriere-hero.png)" >
         <Fade triggerOnce={true} delay={100}>
           <Main maxW="full">
-            <Box maxW="full" py={[12, 20, 32]}>
+            <Box maxW="full" py={[12, 20, 32]} w="800px">
               <Heading
                 maxW="full"
                 as="h1"
@@ -96,87 +96,85 @@ export default function Home() {
               <Grid templateColumns={["repeat(7, 1fr)"]}
                       gap={[10,10]}>
 
-                            <GridItem pos="relative"> {/*---location Icon----*/}
-                              <Center h="100%">
-                                <NextImage 
-                                  src="/assets/icons/location-icon.png"
-                                  height="41px"
-                                  width="30px"
-                                  />
-                                </Center>
-                            </GridItem>
+                <GridItem pos="relative"> {/*---location Icon----*/}
+                  <Center h="100%">
+                    <NextImage 
+                      src="/assets/icons/location-icon.png"
+                      height="41px"
+                      width="30px"
+                      />
+                    </Center>
+                </GridItem>
 
-                            <GridItem pos="relative" colSpan={[1,null,6]}> {/*---Text 1. Reihe----*/}
-                              <Text fontSize="24px"> <strong>PlanB. GmbH</strong> <br></br>Kocherstrasse 10<br></br>DE-73460 Hüttlingen</Text> 
-                            </GridItem>
-                          
-                          
-                            <GridItem pos="relative">{/*---phone Icon----*/}
-                              <Center h="100%">
-                                <NextImage 
-                                  src="/assets/icons/phone-icon.png"
-                                  height="30px"
-                                  width="30px"
-                                  />
-                              </Center>
-                            </GridItem>
-                            
-                            <GridItem pos="relative" colSpan={[1,null,6]}> {/*---Text 2. Reihe----*/} 
-                                <Text fontSize="24px">+49 7361 55 62 10</Text> 
-                            </GridItem>
-                          
+                <GridItem pos="relative" colSpan={[1,null,6]}> {/*---Text 1. Reihe----*/}
+                  <Text fontSize="24px"> <strong>PlanB. GmbH</strong> <br></br>Kocherstrasse 10<br></br>DE-73460 Hüttlingen</Text> 
+                </GridItem>
+              
+              
+                <GridItem pos="relative">{/*---phone Icon----*/}
+                  <Center h="100%">
+                    <NextImage 
+                      src="/assets/icons/phone-icon.png"
+                      height="30px"
+                      width="30px"
+                      />
+                  </Center>
+                </GridItem>
+                
+                <GridItem pos="relative" colSpan={[1,null,6]}> {/*---Text 2. Reihe----*/} 
+                    <Text fontSize="24px">+49 7361 55 62 10</Text> 
+                </GridItem>
+              
 
-                          
-                            <GridItem pos="relative">{/*---mail Icon----*/}
-                              <Center h="100%"> 
-                                <NextImage 
-                                  src="/assets/icons/mail-icon.png"
-                                  height="21px"
-                                  width="30px"
-                                  />
-                              </Center>
-                            </GridItem>
+              
+                <GridItem pos="relative">{/*---mail Icon----*/}
+                  <Center h="100%"> 
+                    <NextImage 
+                      src="/assets/icons/mail-icon.png"
+                      height="21px"
+                      width="30px"
+                      />
+                  </Center>
+                </GridItem>
 
-                            <GridItem pos="relative" colSpan={[1,null,6]} > {/*---Text 3. Reihe----*/}
-                                <Text fontSize="24px">info@plan-b-gmbh.com</Text> 
-                            </GridItem>
-                          
+                <GridItem pos="relative" colSpan={[1,null,6]} > {/*---Text 3. Reihe----*/}
+                    <Text fontSize="24px">info@plan-b-gmbh.com</Text> 
+                </GridItem>
+              
 
-                          {/*------------------------Social Media Icons------------------------*/}
-                          
+              {/*------------------------Social Media Icons------------------------*/}
 
-                          
-                            <GridItem pos="relative"></GridItem>
-                            <GridItem pos="relative">
-                              <NextImage 
-                                src="/assets/icons/facebook-icon.png"
-                                height="30px"
-                                width="30px"
-                                />
-                            </GridItem>
-                            <GridItem pos="relative">
-                              <NextImage 
-                                  src="/assets/icons/instagram-icon.png"
-                                  height="30px"
-                                  width="30px"
-                                  />
-                            </GridItem>
-                            <GridItem pos="relative">
-                            <NextImage 
-                                  src="/assets/icons/twitter-icon.png"
-                                  height="30px"
-                                  width="30px"
-                                  />
-                            </GridItem>
-                            <GridItem pos="relative">
-                              <NextImage 
-                                    src="/assets/icons/linkedin-icon.png"
-                                    height="30px"
-                                    width="30px"
-                                    />
-                            </GridItem>
-                            <GridItem pos="relative"> </GridItem>
-                            <GridItem pos="relative"> </GridItem>
+                <GridItem pos="relative"></GridItem>
+                <GridItem pos="relative">
+                  <NextImage 
+                    src="/assets/icons/facebook-icon.png"
+                    height="30px"
+                    width="30px"
+                    />
+                </GridItem>
+                <GridItem pos="relative">
+                  <NextImage 
+                      src="/assets/icons/instagram-icon.png"
+                      height="30px"
+                      width="30px"
+                      />
+                </GridItem>
+                <GridItem pos="relative">
+                <NextImage 
+                      src="/assets/icons/twitter-icon.png"
+                      height="30px"
+                      width="30px"
+                      />
+                </GridItem>
+                <GridItem pos="relative">
+                  <NextImage 
+                        src="/assets/icons/linkedin-icon.png"
+                        height="30px"
+                        width="30px"
+                        />
+                </GridItem>
+                <GridItem pos="relative"> </GridItem>
+                <GridItem pos="relative"> </GridItem>
               </Grid>
             </GridItem>
 
@@ -188,14 +186,14 @@ export default function Home() {
                 <GridItem pos="relative"> 
                   <FormControl id="name">
                   <FormLabel>NAME</FormLabel>
-                  <Input type="name" placeholder="Max Mustermann"/>
+                  <Input type="name" placeholder="Max Mustermann" borderRadius="0" borderColor="white"/>
                   </FormControl>
                 </GridItem>
 
                 <GridItem pos="relative"> 
                   <FormControl id="email">
                   <FormLabel>E-MAIL-ADRESSE</FormLabel>
-                  <Input type="email" placeholder="max@adresse.com"/>
+                  <Input type="email" placeholder="max@adresse.com"  borderRadius="0" borderColor="white"/>
                   </FormControl>
                 </GridItem>
 
@@ -204,14 +202,14 @@ export default function Home() {
                 <GridItem pos="relative" colSpan={[1,null,3]}> 
                   <FormControl id="betreff">
                   <FormLabel>BETREFF</FormLabel>
-                  <Input type="betreff" placeholder="Bitte geben Sie einen Betreff ein..."/>
+                  <Input type="betreff" placeholder="Bitte geben Sie einen Betreff ein..."  borderRadius="0" borderColor="white"/>
                   </FormControl>
                 </GridItem>
 
                 <GridItem pos="relative" colSpan={[1,null,3]} rowSpan={[1,null,2]} >
                   <FormControl id="nachricht">
                   <FormLabel size="185">NACHRICHT</FormLabel>
-                  <Input type="nachricht" h="185" placeholder="Bitte geben Sie eine Nachricht ein..."/>
+                  <Input type="nachricht" h="185" placeholder="Bitte geben Sie eine Nachricht ein..."  borderRadius="0" borderColor="white"/>
                   </FormControl>
                 </GridItem>
 
