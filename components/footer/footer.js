d8255859320977001b7c35724015aa12a661ab1f
philippreiner <phil@q-fx.de>
2021-10-01 15:45:04 +0200

    Refactor Footer icon spacing

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,17 +2,13 @@
 import {
     Box,
     Button,
-    IconButton,
     useColorMode,
     Container,
     Flex,
     Spacer,
     Stack,
-    Tooltip,
-    Text,
     Image
 } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 import NextLink from "next/link";
 import Style from "./footer.module.css";
 import { Divided } from "@components/blocks/blocks";
@@ -23,40 +19,29 @@ export default function Posthead({ title, published }) {
     return (
         <Divided>
             <Container maxW={["100em", null, null, null, "90%"]} pt={[4, 12]} pb={[0, 0]}>
-                <Flex as="header" align={["flex-start", "center"]} direction={["column", "row"]}>
-                    <Stack direction={["column-reverse", "row"]} spacing={[2, 8]} alignItems={["baseline", "center"]} w="100%">
+                <Flex as="footer" align={["flex-start",null,"center"]} direction={["column-reverse", null, "row"]}>
+                    <Flex w="100%" grow={null,null,1} direction={["row"]} align={["center"]}>
+                        <NextLink passHref href="/">
+                            <a>
+                                <Image src="/assets/planb-logo.png" alt="PlanB." height="100px" mr="40px" />
+                            </a>
+                        </NextLink>
+                        <Spacer/>
+                        <Stack direction={["row"]} mt={[null,null,"-8px"]} spacing="16px">
+                            <a target="_blank" href="https://www.linkedin.com/company/planbgmbh/"><Image src="/assets/icons/linkedin-icon-296px.png" width="16px"/></a>
+                            <a target="_blank" href="https://twitter.com/planbgmbh"><Image src="/assets/icons/twitter-icon-296px.png" width="16px"  /></a>
+                            <a target="_blank" href="https://www.instagram.com/planb.gmbh/"><Image src="/assets/icons/instagram-icon-296px.png" width="16px" /></a>
+                            <a target="_blank" href="https://www.facebook.com/PlanBGmbH"><Image src="/assets/icons/facebook-icon-296px.png" width="16px"/></a>
+                        </Stack>
+                    </Flex>
 
-                        <Box pb={[0, 0]} d="flex" alignItems="center" width="100%">
-                            <NextLink href="/">
-                                <a>
-                                    <Image src="/assets/planb-logo.png" alt="PlanB." height="100px" mr="40px" />
-                                </a>
-                            </NextLink>
-                            <Text display={["none", "inline"]} width="300px">© 2021 PlanB.GmbH</Text>
-                            <Box display={["inline-flex", "none"]}>
-                                <a target="_blank" href="https://www.linkedin.com/company/planbgmbh/"><Image src="/assets/icons/linkedin-icon-296px.png" width="16px" ml="16px"/></a>
-                                <a target="_blank" href="https://twitter.com/planbgmbh"><Image src="/assets/icons/twitter-icon-296px.png" width="16px" ml="16px" /></a>
-                                <a target="_blank" href="https://www.instagram.com/planb.gmbh/"><Image src="/assets/icons/instagram-icon-296px.png" width="16px" ml="16px" /></a>
-                                <a target="_blank" href="https://www.facebook.com/PlanBGmbH"><Image src="/assets/icons/facebook-icon-296px.png" width="16px" ml="16px"/></a>
-                            </Box>
-                        </Box>
-
-                        <Spacer />
-
-                        <Box display={["none", "contents"]}>
-                            <a target="_blank" href="https://www.linkedin.com/company/planbgmbh/"><Image src="/assets/icons/linkedin-icon-296px.png" width="16px" ml="16px" /></a>
-                            <a target="_blank" href="https://twitter.com/planbgmbh"><Image src="/assets/icons/twitter-icon-296px.png" width="16px" ml="16px" /></a>
-                            <a target="_blank" href="https://www.instagram.com/planb.gmbh/"><Image src="/assets/icons/instagram-icon-296px.png" width="16px" ml="16px" /></a>
-                            <a target="_blank" href="https://www.facebook.com/PlanBGmbH"><Image src="/assets/icons/facebook-icon-296px.png" width="16px" ml="16px" /></a>
-                        </Box>
-
+                    <Stack ml={[null,null,"16px"]} direction={["row-reverse","row"]} mb={2}spacing="16px">
                         <NextLink href="/datenschutz" passHref>
                             <Button variant="ghost">Datenschutz</Button>
                         </NextLink>
                         <NextLink href="/imprint" passHref>
                             <Button variant="ghost">Impressum</Button>
                         </NextLink>
-
                     </Stack>
                 </Flex>
             </Container>
