d8a8e7c4ef6e6f9fdbb55dcf3f0ca1f850574c3c
benjamin.schulz <benjamin.schulz@plan-b-gmbh.com>
2023-02-09 15:56:45 +0100

    updated header config

--- next.config.js
+++ next.config.js
@@ -87,7 +87,7 @@ module.exports = {
     async headers() {
         return [
             {
-                source: '*',
+                source: '/*',
                 headers: [
                     { 
                         key: 'X-Frame-Options',
